687b787be03812c1c0ecd85d3db33443be30b030
Leo Bastos <57710891+LeoBastos@users.noreply.github.com>
2023-02-03 16:27:18 -0300

    Update mypaths.py

--- src/config/mypaths.py
+++ src/config/mypaths.py
@@ -1,3 +1,3 @@
 path = '/home/leonardo/PycharmProjects/Projeto/src/logs'
 path_modulo = '/home/leonardo/Área de Trabalho/exec/dist/filho/'
-url_update = 'https://tinyurl.com/yh7pjwdy'
+url_update = '********'
